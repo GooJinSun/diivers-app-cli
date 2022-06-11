c3d13fc5f8fbaa07ef1a049be00d731beb6d5b9e
Gina Park <koyrkr@gmail.com>
2022-06-12 02:44:52 +0900

    fix: finish test getting & sending message

--- App.js
+++ App.js
@@ -1,42 +1,48 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import type { Node } from 'react';
-import { SafeAreaView, StatusBar, useColorScheme } from 'react-native';
+import { Button, SafeAreaView, StatusBar } from 'react-native';
 import { WebView } from 'react-native-webview';
 import { Colors } from 'react-native/Libraries/NewAppScreen';
 import AsyncStorage from '@react-native-community/async-storage';
 import * as WebBrowser from 'expo-web-browser';
 
 const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
   const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.WebView,
+    backgroundColor: Colors.WebView,
     flex: 1,
   };
 
-  const handleNativeEvent = async (event: WebViewMessageEvent) => {
+  let webViewRef = useRef(null);
+
+  const handleSetRef = (ref) => {
+    webViewRef = ref;
+  };
+
+  const handleNativeEvent = async ({ nativeEvent: { data } }) => {
+    console.log(data);
     // Function that is invoked when the webview calls
-    const data = JSON.parse(event.nativeEvent.data);
+    const _data = JSON.parse(data);
 
     // 웹에서의 console.log를 앱 터미널에서도 볼 수 있도록
-    if (data) {
-      if (data.type === 'Console') {
-        console.info(`[Console From Webview] ${JSON.stringify(data.data)}`);
+    if (_data) {
+      console.log(_data);
+      if (_data.type === 'Console') {
+        console.info(`[Console From Webview] ${JSON.stringify(_data.data)}`);
       } else {
-        console.log(data);
+        console.log(_data);
       }
     }
 
-    switch (data.actionType) {
+    switch (_data.actionType) {
       // 브라우저 따로 열어야할 때 (ex. 개인정보처리방침 등)
       case 'OPEN_BROWSER':
-        return WebBrowser.openBrowserAsync(data.url);
+        return WebBrowser.openBrowserAsync(_data.url);
       // NOTE: 로컬 스토리지 관리
       case 'SET_ASYNC_STORAGE':
-        if (typeof data.value === 'string') {
-          await AsyncStorage.setItem(data.key, data.value);
+        if (typeof _data.value === 'string') {
+          await AsyncStorage.setItem(_data.key, _data.value);
         } else {
-          await AsyncStorage.setItem(data.key, JSON.stringify(data.value));
+          await AsyncStorage.setItem(_data.key, JSON.stringify(_data.value));
         }
         break;
       default:
@@ -46,22 +52,20 @@ const App: () => Node = () => {
 
   return (
     <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <StatusBar />
       <WebView
         onMessage={handleNativeEvent}
-        ref={(webView) => (this.webView = webView)}
-        onLoadProgress={(e: WebViewMessageEvent) => {
-          // Function that is invoked when the WebView is loading.
+        ref={webViewRef}
+        source={{ uri: 'http://localhost:3000' }}
+        // source={{ uri: 'https://adoor.world' }}
+        handleSetRef={handleSetRef}
+      />
+      <Button
+        onPress={() => {
+          console.log('send message');
+          webViewRef.current.postMessage('RN으로부터 보내는 메시지');
         }}
-        source={{ uri: 'https://adoor.world' }}
-        onContentProcessDidTerminate={() => {
-          // Function that is invoked when the WebKit WebView content process gets terminated.
-          try {
-            this.webview.reload();
-          } catch (e) {
-            this._reload();
-          }
-        }}
+        title={'버튼'}
       />
     </SafeAreaView>
   );
