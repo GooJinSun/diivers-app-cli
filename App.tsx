434a2817cfb50a21af6663a7add84b17b174670a
koyrkr <koyrkr@gmail.com>
2023-03-18 16:02:47 +0900

    fix: add react-native stack for safe-area view

--- App.tsx
+++ App.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { WebViewScreen } from '@screens';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
 
 const App: React.FC = () => {
   return (
     <SafeAreaProvider>
-      <WebViewScreen />
+      <NavigationContainer>
+        <Stack.Navigator
+          screenOptions={{
+            headerShown: false,
+          }}
+        >
+          <Stack.Screen name="WebViewScreen" component={WebViewScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </SafeAreaProvider>
   );
 };
